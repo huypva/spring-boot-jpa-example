f0472c4cd1fd44ca7e46296b79aad3631a6e54c3
huypva <huypva@vng.com.vn>
2021-10-22 13:19:10 +0700

    Update unit-test

--- spring-boot-jpa/src/test/java/io/codebyexample/springbootjpa/core/service/UserServiceImplTest.java
+++ spring-boot-jpa/src/test/java/io/codebyexample/springbootjpa/core/service/UserServiceImplTest.java
@@ -1,15 +1,13 @@
 package io.codebyexample.springbootjpa.core.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.BDDMockito.given;
 
-import io.codebyexample.springbootjpa.configuration.MySQLTestContainer;
 import io.codebyexample.springbootjpa.dataprovider.user.UserDto;
 import io.codebyexample.springbootjpa.dataprovider.user.UserRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
@@ -31,7 +29,8 @@ public class UserServiceImplTest {
   @Test
   void givenExistingUserWhenGetUserByUserNameThenReturnUser() {
     UserDto userDefault = new UserDto(1, "Huy");
-    given(userRepository.findUserByUserName("Huy")).willReturn(Optional.of(userDefault));
+
+    Mockito.when(userRepository.findUserByUserName("Huy")).thenReturn(Optional.of(userDefault));
 
     UserDto user = userService.getUserByName("Huy");
     assertThat(user).usingRecursiveComparison().isEqualTo(userDefault);
--- spring-boot-jpa/src/test/java/io/codebyexample/springbootjpa/dataprovider/user/UserRepositoryTest.java
+++ spring-boot-jpa/src/test/java/io/codebyexample/springbootjpa/dataprovider/user/UserRepositoryTest.java
@@ -1,18 +1,14 @@
 package io.codebyexample.springbootjpa.dataprovider.user;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 import io.codebyexample.springbootjpa.configuration.MySQLTestContainer;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
 import org.springframework.test.context.ActiveProfiles;
@@ -27,16 +23,13 @@ import org.springframework.test.context.ActiveProfiles;
 public class UserRepositoryTest extends MySQLTestContainer {
 
   @Autowired
-  private TestEntityManager entityManager;
-
-  @Autowired
   private UserRepository userRepository;
 
   @Test
-  void givenExistingUserUsernameWhenFindUserByUsernameThenReturnUser() {
+  void findUserByUserName_findExistedUser() {
     UserDto user = new UserDto(1, "Huy");
 
-    Optional<UserDto> userOptional = userRepository.findUserByUserName(user.getUserName());
+    Optional<UserDto> userOptional = userRepository.findUserByUserName("Huy");
 
     assertThat(userOptional).isPresent();
     assertThat(userOptional.get()).usingRecursiveComparison().isEqualTo(user);
